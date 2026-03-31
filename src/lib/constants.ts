dfcda896568f81d623063e91ec2d73e0e07e104b
RidhoIT <ridho22ti@mahasiswa.pcr.ac.id>
2026-03-31 20:13:51 +0700

    update constants dan schema

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,7 +6,8 @@ export const ADMIN_PASSWORD = "112233";
 export const PASSING_GRADE = 70;
 
 export const DEFAULT_CONFIG: ExamConfig = {
-  exam_title: "ASESMEN SUMATIF AKHIR SEMESTER GENAP KELAS XII",
+  // exam_title: "ASESMEN SUMATIF AKHIR SEMESTER GENAP KELAS XII",
+  exam_title: "PENILAIAN SUMATIF AKHIR JENJANG",
   subject: "MATA PELAJARAN INFORMATIKA",
   academic_year: "TAHUN PELAJARAN 2025/2026",
   duration_minutes: 90,
